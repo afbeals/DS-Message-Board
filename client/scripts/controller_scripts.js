c1a8a2fb44a91698ec2c151edc6bddbcb9f4fa1d
afbeals <afbeals@hotmail.com>
2016-06-26 15:42:10 -0800

    update frontend controller to pull topic data

--- client/scripts/controller_scripts.js
+++ client/scripts/controller_scripts.js
@@ -1,1 +1,42 @@
-controller_scripts.js
+// Angular Controller
+// Dashboard Controller
+  DiscussionBoard.controller('DashboardController', function($scope, UsersFactory, TopicsFactory)
+  {
+
+
+    
+  });
+
+// Topic Controller
+  DiscussionBoard.controller('TopicController', function($scope, UsersFactory, TopicsFactory)
+  {
+    // create arrays to pull data from database and have front end cycle through and display each item in array
+    var users = [];
+    var topics = [];
+    var answer = [];
+    var comment = [];
+    // temp currentUser variable to track user, future change to object similar to session
+    var currentUser = ''; 
+
+    // use scope to access global functions made available by angular
+    $scope.addTopic = function()
+    {
+      // console log data being pulled to ensure data will look as expected correct type
+      console.log($scope.add_topic);
+      // old functions will update to fit procject v
+      // TopicsFactory.addTopic($scope.add_topic, function()
+      // {
+      //   TopicsFactory.getTopics(function(data)
+      //   {
+      //     $scope.topics = data;
+      //   })
+      // })
+      // UsersFactory.addUser($scope.add_topic, function()
+      //   {
+      //   });
+
+      // clear object to make it ready for next added topic
+      $scope.add_topic={};
+    }
+  
+  });
--- client/scripts/routing_scripts.js
+++ client/scripts/routing_scripts.js
@@ -3,5 +3,6 @@
     $routeProvider
       .when('/', {templateUrl: 'partials/welcome_partial.html', controller: 'WelcomeController'})
       .when('/home', {templateUrl: 'partials/home_partial.html', controller: 'HomeController'})
+      .when('/topic', {templateUrl: 'partials/topics_partial.html', controller: 'TopicController'})
       .otherwise({redirectTo: '/'});
   });
